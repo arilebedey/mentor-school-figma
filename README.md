a5466148b520dfb96413a5368ab57955fb797546
arilebedey <34816154+arilebedey@users.noreply.github.com>
2024-07-09 21:39:51 +0200

    Added Navigation Bar

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 https://www.figma.com/design/l3lyICTpyhFnrC9O3O58Ww/%D0%BD%D0%B0%D1%81%D1%82%D0%B0%D0%B2%D0%BD%D0%B8%D1%87%D0%B5%D1%81%D1%82%D0%B2%D0%BE?node-id=561-5549&t=ZafIQSZEgDyZSlrs-0
 
-delete ./.git
+https://github.com/arilebedey/mentor-school-figma
---
+++ mentor-school-page/src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Navbar.css";
+import logo from "../assets/NavBar/logo.png";
+
+const Navbar: React.FC = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar-logo">
+        <img src={logo} alt="Logo" /> // WORKING
+        {/* <img src="../assets/NavBar/logo.png" alt="Logo" /> // NOT WORKING */}
+      </div>
+      <div className="navbar-links">
+        <a href="#news">НОВОСТИ</a>
+        <a href="#contestants">ЛИЦА КОНКУРСА</a>
+        <a href="#criteria">КРИТЕРИИ ЗАЩИТЫ ПРОЕКТОВ</a>
+        <a href="#how-to-join">КАК СТАТЬ СТУДЕНТОМ?</a>
+        <a href="#contacts">КОНТАКТЫ</a>
+      </div>
+      <div className="navbar-apply">
+        <button>ПОДАТЬ ЗАЯВКУ</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
